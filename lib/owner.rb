af5a92c61cbd2f91637f303e3898a61a47c1a003
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-06-12 01:13:17 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,37 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species
+  attr_accessor :cat
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @species = species
+    @@all << self
+    @cat = []
+  end
+
+  def species
+    @species = "human"
+  end
+  
+  def say_species
+    "I am a #{species}."
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@all.length
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
+  
+
+    
+  
 end
